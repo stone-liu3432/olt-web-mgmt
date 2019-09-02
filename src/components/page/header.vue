bff8847b8810424a73c993c724f4a49cd217d701
stone-liu3432 <liushuang3432@163.com>
2019-09-02 14:57:02 +0800

    logo栏，字体长度溢出时显示省略号

--- src/components/page/header.vue
+++ src/components/page/header.vue
@@ -30,19 +30,26 @@
           </div>
       </div>
       <confirm :tool-tips="lanMap['logout'] + '?'" @choose="result" v-if="login_out_modal"></confirm>
-    </div> -->
+    </div>-->
     <div class="top-banner" v-if="system && system.data">
-        <div class="top-banner-logo lf" :style="{ 'font-size': system.data.vendor.length > 12 ? '28px' : '32px' }">
-            <img v-if="has_logo" ref="logo-image" @error="isNoLogo">
-            <a href="javascript: void(0);" v-else>{{ system.data.vendor ? system.data.vendor.length > 18 ? system.data.vendor.substring(0, 18) : system.data.vendor : "Neutral"  }}</a>
+        <div
+            class="top-banner-logo lf"
+            :style="{ 'font-size': system.data.vendor.length > 12 ? '28px' : '32px' }"
+        >
+            <img v-if="has_logo" ref="logo-image" @error="isNoLogo" />
+            <a
+                href="javascript: void(0);"
+                v-else
+            >{{ system.data.vendor ? system.data.vendor.length > 18 ? system.data.vendor.substring(0, 18) : system.data.vendor : "Neutral" }}</a>
         </div>
         <div class="top-banner-nav lf">
             <ul>
-                <li v-for="(item,index) in f_menu" :key="index"
+                <li
+                    v-for="(item,index) in f_menu"
+                    :key="index"
                     @click="nav_click(item)"
-                    :class="{ 'active' : item === nav_menu }">
-                    {{ lanMap[item] }}
-                </li>
+                    :class="{ 'active' : item === nav_menu }"
+                >{{ lanMap[item] }}</li>
             </ul>
         </div>
         <div class="top-banner-user lf">
@@ -53,201 +60,226 @@
             </ul>
         </div>
         <confirm :tool-tips="lanMap['logout'] + '?'" @choose="result" v-if="login_out_modal"></confirm>
-        <confirm :tool-tips="lanMap['save_cfg_confirm']" @choose="savecfg_result" v-if="savecfgConfirm"></confirm>
+        <confirm
+            :tool-tips="lanMap['save_cfg_confirm']"
+            @choose="savecfg_result"
+            v-if="savecfgConfirm"
+        ></confirm>
         <loading class="load" v-if="isLoading"></loading>
     </div>
 </template>
 
 <script>
-import { mapState,mapMutations } from 'vuex'
-const f_menu = ['status','onu_allow','vlan_mgmt','advanced_setting']
-import loading from '@/components/common/loading'
+import { mapState, mapMutations } from "vuex";
+const f_menu = ["status", "onu_allow", "vlan_mgmt", "advanced_setting"];
+import loading from "@/components/common/loading";
 export default {
-   name: 'topBanner',
-   computed: mapState(['system','lanMap','language','menu','change_url','nav_menu']),
-   data(){
-       return {
-           //lang: '',
-           uName: '',
-           login_out_modal: false,
-           has_logo: true,
-           f_menu: f_menu,
-           savecfgConfirm: false,
-           isLoading: false
-       }
-   },
-   components: { loading },
-   created(){
-       this.uName = sessionStorage.getItem('uname');
-       this.lang = this.language;
-       var first_menu = sessionStorage.getItem('f_menu');
-       if(first_menu){
-           this.changeMenu(first_menu);
-       }else{
-           this.changeMenu('status');
-       }
-   },
-   methods: {
+    name: "topBanner",
+    computed: mapState([
+        "system",
+        "lanMap",
+        "language",
+        "menu",
+        "change_url",
+        "nav_menu"
+    ]),
+    data() {
+        return {
+            //lang: '',
+            uName: "",
+            login_out_modal: false,
+            has_logo: true,
+            f_menu: f_menu,
+            savecfgConfirm: false,
+            isLoading: false
+        };
+    },
+    components: { loading },
+    created() {
+        this.uName = sessionStorage.getItem("uname");
+        this.lang = this.language;
+        var first_menu = sessionStorage.getItem("f_menu");
+        if (first_menu) {
+            this.changeMenu(first_menu);
+        } else {
+            this.changeMenu("status");
+        }
+    },
+    methods: {
         ...mapMutations({
-            addmenu: 'updateMenu',
-            changeMenu: 'updateNavMenu',
-            changeAdvMenu: 'updateAdvMenu',
-            changeFMenu: 'updateAdvFMenu'
+            addmenu: "updateMenu",
+            changeMenu: "updateNavMenu",
+            changeAdvMenu: "updateAdvMenu",
+            changeFMenu: "updateAdvFMenu"
         }),
-        login_out(){
+        login_out() {
             this.login_out_modal = true;
         },
         //  退出登录
-        result(bool){
-            if(bool){
+        result(bool) {
+            if (bool) {
                 var post_params = {
-                    "method":"set",
-                    "param":{
-                        "name": this.uName
+                    method: "set",
+                    param: {
+                        name: this.uName
                     }
-                }
-                this.$http.post('/userlogin?form=logout',post_params).then(res=>{
-                    this.$message({
-                        type: 'success',
-                        text: this.lanMap['login_out']
+                };
+                this.$http
+                    .post("/userlogin?form=logout", post_params)
+                    .then(res => {
+                        this.$message({
+                            type: "success",
+                            text: this.lanMap["login_out"]
+                        });
+                        sessionStorage.removeItem("x-token");
+                        this.$router.push("/login");
                     })
-                    sessionStorage.removeItem('x-token');
-                    this.$router.push('/login');
-                }).catch(err=>{
-                    // to do
-                })
+                    .catch(err => {
+                        // to do
+                    });
             }
             this.login_out_modal = false;
         },
-        nav_click(node){
+        nav_click(node) {
             this.$router.push(node);
-            if(node === 'advanced_setting'){
-                this.$http.get(this.change_url.menu).then(res=>{
-                    if(res.data.code === 1){
-                        this.addmenu(res.data);
-                    }
-                }).catch(err=>{
-                    // to do
-                })
+            if (node === "advanced_setting") {
+                this.$http
+                    .get(this.change_url.menu)
+                    .then(res => {
+                        if (res.data.code === 1) {
+                            this.addmenu(res.data);
+                        }
+                    })
+                    .catch(err => {
+                        // to do
+                    });
             }
             this.changeMenu(node);
-            sessionStorage.setItem('f_menu', node);
+            sessionStorage.setItem("f_menu", node);
         },
-        savecfg(){
+        savecfg() {
             this.savecfgConfirm = true;
         },
-        savecfg_result(bool){
-            if(bool){
-                this.$http.get('/system_save').then(res=>{
-                    if(res.data.code === 1){
-                        this.$message({
-                            type: res.data.type,
-                            text: this.lanMap['save_succ']
-                        })
-                    }else if(res.data.code > 1){
-                        this.$message({
-                            type: res.data.type,
-                            text: '(' + res.data.code + ') ' + res.data.message
-                        })
-                    }
-                }).catch(err=>{
-                    // to do
-                })
+        savecfg_result(bool) {
+            if (bool) {
+                this.$http
+                    .get("/system_save")
+                    .then(res => {
+                        if (res.data.code === 1) {
+                            this.$message({
+                                type: res.data.type,
+                                text: this.lanMap["save_succ"]
+                            });
+                        } else if (res.data.code > 1) {
+                            this.$message({
+                                type: res.data.type,
+                                text:
+                                    "(" +
+                                    res.data.code +
+                                    ") " +
+                                    res.data.message
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        // to do
+                    });
             }
             this.savecfgConfirm = false;
         },
-        user_mgmt(){
-            this.$router.push('user_mgmt');
+        user_mgmt() {
+            this.$router.push("user_mgmt");
         },
-        isNoLogo(){
+        isNoLogo() {
             this.has_logo = false;
         }
     },
     watch: {
-        'system'(){
+        system() {
             this.$nextTick(_ => {
-                this.$refs['logo-image'].src = '../../logo.png';
-            })
+                this.$refs["logo-image"].src = "../../logo.png";
+            });
         }
     }
-}
+};
 </script>
 
 <style scoped lang="less">
-    .top-banner{
-        width:100%;
-        min-width: 1280px;
-        max-width: 1980px;
-        height:70px;
-        position:fixed;
-        top:0;
-        left:0;
-        z-index: 999;
-        background: #184777;
-        color: #fff;
-        div.top-banner-logo,div.top-banner-nav,div.top-banner-user{
-            height: 70px;
-            line-height: 70px;
-            text-align: center;
-        }
-        div.top-banner-logo{
-            font-size: 32px;
-            font-weight: 600;
-        }
-        ul{
-            li{
-                float: left;
-                cursor: pointer;
-                user-select: none;
-                &:hover{
-                    color: rgb(204, 131, 102);
-                    transition: all .3s linear;
-                }
-            }
-            li.active:hover{
-                background: #e0efe7;
-                color: #000;
-                transition: none;
-            }
-            &::after{
-                content: "";
-                display: table;
-                clear: both;
-            }
-        }
+.top-banner {
+    width: 100%;
+    min-width: 1280px;
+    max-width: 1980px;
+    height: 70px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    background: #184777;
+    color: #fff;
+    div.top-banner-logo,
+    div.top-banner-nav,
+    div.top-banner-user {
+        height: 70px;
+        line-height: 70px;
+        text-align: center;
     }
-    .top-banner::after{
-        content : '';
-        display: table;
-        clear:both;
+    div.top-banner-logo {
+        font-size: 32px;
+        font-weight: 600;
     }
-    div.top-banner-logo{
-        width: 20%;
-    }
-    div.top-banner-nav{
-        width: 60%;
-        li{
-            width: 20%;
-        }
-    }
-    div.top-banner-user{
-        width: 20%;
-        li{
-            width: 33%;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            &:hover{
+    ul {
+        li {
+            float: left;
+            cursor: pointer;
+            user-select: none;
+            &:hover {
                 color: rgb(204, 131, 102);
-                transition: all .3s linear;
+                transition: all 0.3s linear;
             }
         }
+        li.active:hover {
+            background: #e0efe7;
+            color: #000;
+            transition: none;
+        }
+        &::after {
+            content: "";
+            display: table;
+            clear: both;
+        }
     }
-    li.active{
-        background: #e0efe7;
-        color: #000;
+}
+.top-banner::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+div.top-banner-logo {
+    width: 20%;
+}
+div.top-banner-nav {
+    width: 60%;
+    li {
+        width: 20%;
     }
+}
+div.top-banner-user {
+    width: 20%;
+    li {
+        width: 33%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        &:hover {
+            color: rgb(204, 131, 102);
+            transition: all 0.3s linear;
+        }
+    }
+}
+li.active {
+    background: #e0efe7;
+    color: #000;
+}
 
-  
 //   .top-banner h1{
 //     font-size:42px;
 //     line-height:70px;
@@ -260,18 +292,26 @@ export default {
 //     text-align: center;
 //     padding: 0 20px;
 //   }
-  .top-banner-logo a{
-    font-size:42px;
-    font-weight:bold;
-    color:#fff;
+.top-banner-logo a {
+    border: none;
+    font-size: 42px;
+    font-weight: bold;
+    color: #fff;
     background: transparent;
-    &:active{
+    padding: 0;
+    width: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &:active {
         border: none;
+        outline: none;
     }
-    &:focus{
+    &:focus {
         border: none;
+        outline: none;
     }
-  }
+}
 //   .product-type{
 //     width:200px;
 //     height:70px;
