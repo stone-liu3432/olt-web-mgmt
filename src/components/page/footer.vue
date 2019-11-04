a5e010c08acea0ba52b3e2b0660a3b2137364e20
stone-liu3432 <liushuang3432@163.com>
2019-11-04 16:16:24 +0800

    切换中英文时，将选择的语言存储到设备上

--- src/components/page/footer.vue
+++ src/components/page/footer.vue
@@ -2,52 +2,62 @@
     <div id="footer">
         <div>
             <span>{{ lanMap['lang'] }}</span>
-            <span> : </span>
-            <span @click="changeLang('zh')"
-                :style="{ 'color' : language === 'zh' ? '#67aef6' : '' }">
-                简体中文
-            </span>
-            <span @click="changeLang('en')"
-                :style="{ 'color' : language === 'en' ? '#67aef6' : '' }">
-                English
-            </span>
+            <span>:</span>
+            <span
+                @click="changeLang('zh')"
+                :style="{ 'color' : language === 'zh' ? '#67aef6' : '' }"
+            >简体中文</span>
+            <span
+                @click="changeLang('en')"
+                :style="{ 'color' : language === 'en' ? '#67aef6' : '' }"
+            >English</span>
             <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
             <span>{{ lanMap['fw_ver'] }}</span>
-            <span> : </span>
-            <span v-if="system && system.data">
-                {{ system.data.fw_ver }}
-            </span>
+            <span>:</span>
+            <span v-if="system && system.data">{{ system.data.fw_ver }}</span>
             <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
             <span>{{ lanMap['macaddr'] }}</span>
-            <span> : </span>
+            <span>:</span>
             <span v-if="system && system.data">{{ system.data.macaddr }}</span>
         </div>
     </div>
 </template>
 
 <script>
-import { mapState,mapMutations } from 'vuex'
+import { mapState, mapMutations } from "vuex";
 export default {
-    name: 'bottomFooter',
-    data(){
+    name: "bottomFooter",
+    data() {
         return {
-            lang: '',
-        }
+            lang: ""
+        };
     },
-    computed: mapState(['lanMap', 'language', 'system']),
+    computed: mapState(["lanMap", "language", "system"]),
     methods: {
         ...mapMutations({
-            change_lang: 'updateLang'
+            change_lang: "updateLang"
         }),
-        changeLang(lang){
+        changeLang(lang) {
             this.change_lang(lang);
+            const data = {
+                method: "set",
+                param: {
+                    lang
+                }
+            };
+            this.$http
+                .post("/system_lang", data)
+                .then(res => {
+                    // to do
+                })
+                .catch(err => {});
         }
     }
-}
+};
 </script>
 
 <style lang="less" scoped>
-#footer{
+#footer {
     position: fixed;
     bottom: 0;
     left: 0;
@@ -56,7 +66,7 @@ export default {
     line-height: 30px;
     text-align: center;
     background: #fff;
-    span{
+    span {
         font-size: 15px;
         cursor: pointer;
     }
--- src/components/page/login.vue
+++ src/components/page/login.vue
@@ -17,23 +17,35 @@
             <form>
                 <div>
                     <span>{{ lanMap['user_name'] }}</span>
-                    <input type="text" v-model="userName" :class="[ verify_uname ? 'input-error' : '' ]" v-focus>
+                    <input
+                        type="text"
+                        v-model="userName"
+                        :class="[ verify_uname ? 'input-error' : '' ]"
+                        v-focus
+                    />
                 </div>
                 <div class="user-pwd">
                     <span>{{ lanMap['password'] }}</span>
-                    <input type="password" 
-                        v-model="userPwd" id="userPwd" :class="[ verify_upwd ? 'input-error' : '' ]" 
-                        autocomplete="off" @keyup.enter="userLogin">
+                    <input
+                        type="password"
+                        v-model="userPwd"
+                        id="userPwd"
+                        :class="[ verify_upwd ? 'input-error' : '' ]"
+                        autocomplete="off"
+                        @keyup.enter="userLogin"
+                    />
                     <i :class="[ visible ? 'visible' : 'invisible']" @click="changeVisible"></i>
                 </div>
                 <div class="login-tips">
-                    <h4 :style="{'opacity': verify_uname ? 1 : 0}">
-                        {{ lanMap['username_length_fail'] }}
-                    </h4>
-                    <h4 :style="{'opacity': verify_upwd ? 1 : 0}">
-                        {{ lanMap['password_length_fail'] }}
-                    </h4>
-                    <h4 :style="{'opacity': login_failed ? 1 : 0}">{{ lanMap['service_user_password_fail'] }}</h4>
+                    <h4
+                        :style="{'opacity': verify_uname ? 1 : 0}"
+                    >{{ lanMap['username_length_fail'] }}</h4>
+                    <h4
+                        :style="{'opacity': verify_upwd ? 1 : 0}"
+                    >{{ lanMap['password_length_fail'] }}</h4>
+                    <h4
+                        :style="{'opacity': login_failed ? 1 : 0}"
+                    >{{ lanMap['service_user_password_fail'] }}</h4>
                 </div>
                 <div>
                     <a href="javascript:;" @click="userLogin">{{ lanMap['login_user'] }}</a>
@@ -44,134 +56,159 @@
 </template>
 
 <script>
-import md5 from 'md5'
-import { mapState,mapMutations } from "vuex"
-    export default {
-        name: 'login',
-        data(){
-            return {
-                userName: '',
-                userPwd: '',
-                //  验证用户名
-                verify_uname: false,
-                //  验证密码
-                verify_upwd: false,
-                //  用户名或密码服务器验证未通过
-                login_failed: false,
-                //  密码可见或不可见
-                visible: false,
-                //  语言选项
-                lang: ''
+import md5 from "md5";
+import { mapState, mapMutations } from "vuex";
+export default {
+    name: "login",
+    data() {
+        return {
+            userName: "",
+            userPwd: "",
+            //  验证用户名
+            verify_uname: false,
+            //  验证密码
+            verify_upwd: false,
+            //  用户名或密码服务器验证未通过
+            login_failed: false,
+            //  密码可见或不可见
+            visible: false,
+            //  语言选项
+            lang: ""
+        };
+    },
+    computed: mapState(["lanMap", "language"]),
+    created() {
+        this.lang = this.language;
+        sessionStorage.clear();
+    },
+    methods: {
+        ...mapMutations({
+            set_language: "updateLang"
+        }),
+        userLogin() {
+            this.login_failed = false;
+            if (
+                this.verify_uname ||
+                this.verify_upwd ||
+                this.userName === "" ||
+                this.userPwd === ""
+            ) {
+                return;
             }
-        },
-        computed: mapState(['lanMap','language']),
-        created(){
-            this.lang = this.language;
-            sessionStorage.clear();
-        },
-        methods: {
-            ...mapMutations({
-                set_language: 'updateLang'
-            }),
-            userLogin(){
-                this.login_failed = false;
-                if(this.verify_uname || this.verify_upwd || this.userName === '' || this.userPwd === ''){
-                    return 
-                }
-                var self = this;
-                this.$http({
-                    url: '/userlogin?form=login',
-                    method: 'POST',
-                    data: {
-                        "method":"set",
-                        "param":{
-                            "name": this.userName,
-                            "key": md5(self.userName + ':' + self.userPwd)
-                        }
-                    },
-                    timeout: 5000
-                }).then(res=>{
-                    this.userPwd = '';
-                    if(res.data.code === 1){
-                        sessionStorage.setItem('x-token',res.headers['x-token']);
-                        sessionStorage.setItem('uname',this.userName);
+            var self = this;
+            this.$http({
+                url: "/userlogin?form=login",
+                method: "POST",
+                data: {
+                    method: "set",
+                    param: {
+                        name: this.userName,
+                        key: md5(self.userName + ":" + self.userPwd)
+                    }
+                },
+                timeout: 5000
+            })
+                .then(res => {
+                    this.userPwd = "";
+                    if (res.data.code === 1) {
+                        sessionStorage.setItem(
+                            "x-token",
+                            res.headers["x-token"]
+                        );
+                        sessionStorage.setItem("uname", this.userName);
                         this.$message({
-                            type: 'success',
-                            text: this.lanMap['login_success'] + ',' + this.userName
-                        })
-                        this.$router.push('/main');
-                    }else if(res.data.code > 1){
+                            type: "success",
+                            text:
+                                this.lanMap["login_success"] +
+                                "," +
+                                this.userName
+                        });
+                        this.$router.push("/main");
+                    } else if (res.data.code > 1) {
                         this.$message({
-                            type: 'error',
-                            text: '(' + res.data.code + ') ' + res.data.message
-                        })
+                            type: "error",
+                            text: "(" + res.data.code + ") " + res.data.message
+                        });
                         this.login_failed = true;
                     }
-                }).catch(err=>{
+                })
+                .catch(err => {
                     // 登录超时时的处理，默认为5秒无响应超时
                     this.$message({
-                        type: 'error',
-                        text: this.lanMap['http_login_timeout']
-                    })
-                })
-            },
-            changeVisible(){
-                this.visible = !this.visible;
-                var uPwd = document.getElementById('userPwd');
-                if(this.visible){
-                    uPwd.type = 'text';
-                }else{
-                    uPwd.type = 'password';
-                }
-            }
+                        type: "error",
+                        text: this.lanMap["http_login_timeout"]
+                    });
+                });
         },
-        watch: {
-            userName(){
-                var reg = /^[a-zA-Z][a-zA-Z_\d]{3,15}$/;
-                if(!reg.test(this.userName)){
-                    this.verify_uname = true;
-                }else{
-                    this.verify_uname = false;
-                }
-                if(this.userName == ''){
-                    this.verify_uname = false;
-                }
-                this.login_failed = false;
-            },
-            userPwd(){
-                var reg = /^[^\s\?\\\/\'\"]{5,16}$/;
-                if(!reg.test(this.userPwd)){
-                    this.verify_upwd = true;
-                }else{
-                    this.verify_upwd = false;
-                }
-                if(this.userPwd == ''){
-                    this.verify_upwd = false;
-                }
-                this.login_failed = false;
-            },
-            lang(){
-                this.set_language(this.lang);
-            },
-            language(){
-                this.lang = this.language;
+        changeVisible() {
+            this.visible = !this.visible;
+            var uPwd = document.getElementById("userPwd");
+            if (this.visible) {
+                uPwd.type = "text";
+            } else {
+                uPwd.type = "password";
             }
         }
+    },
+    watch: {
+        userName() {
+            var reg = /^[a-zA-Z][a-zA-Z_\d]{3,15}$/;
+            if (!reg.test(this.userName)) {
+                this.verify_uname = true;
+            } else {
+                this.verify_uname = false;
+            }
+            if (this.userName == "") {
+                this.verify_uname = false;
+            }
+            this.login_failed = false;
+        },
+        userPwd() {
+            var reg = /^[^\s\?\\\/\'\"]{5,16}$/;
+            if (!reg.test(this.userPwd)) {
+                this.verify_upwd = true;
+            } else {
+                this.verify_upwd = false;
+            }
+            if (this.userPwd == "") {
+                this.verify_upwd = false;
+            }
+            this.login_failed = false;
+        },
+        lang() {
+            this.set_language(this.lang);
+            const data = {
+                method: "set",
+                param: {
+                    lang: this.lang
+                }
+            };
+            this.$http
+                .post("/system_lang", data)
+                .then(res => {
+                    // to do
+                })
+                .catch(err => {});
+        },
+        language() {
+            this.lang = this.language;
+        }
     }
+};
 </script>
 
 <style scoped lang="less">
-div.login{
+div.login {
     width: 100%;
     min-width: 1280px;
     height: 100%;
     min-height: 768px;
-    background: #6C6D69;
+    background: #6c6d69;
 }
-div.login-banner{
+div.login-banner {
     height: 90px;
 }
-div.login-body{
+div.login-body {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -183,66 +220,66 @@ div.login-body{
     border-radius: 8px;
     text-align: center;
 }
-div.login-body>h2{
+div.login-body > h2 {
     font-size: 28px;
     font-weight: 600;
     color: #666;
     height: 50px;
     line-height: 50px;
 }
-div.login-body>h3{
+div.login-body > h3 {
     font-size: 14px;
     height: 30px;
     line-height: 30px;
     color: #49a3ff;
 }
-div.login-tips{
+div.login-tips {
     position: relative;
     text-align: center;
     width: auto;
 }
-div.login-tips>h4{
+div.login-tips > h4 {
     font-size: 14px;
     width: 100%;
     height: 30px;
     line-height: 30px;
     color: red;
 }
-div.login-tips>h4:last-child{
+div.login-tips > h4:last-child {
     position: absolute;
     left: 0;
     top: 0;
 }
-div.login-body a{
+div.login-body a {
     display: inline-block;
     position: relative;
     width: 300px;
     height: 42px;
     line-height: 42px;
     text-align: center;
-    background: #49A3FF;
+    background: #49a3ff;
     color: #fff;
     font-size: 20px;
     border-radius: 5px;
     transition: all 0.3s linear;
 }
-div.login-body a:active{
+div.login-body a:active {
     background: rgb(13, 113, 146);
 }
-form div{
+form div {
     line-height: 50px;
     width: 450px;
     margin: 15px auto;
 }
-div.user-pwd{
+div.user-pwd {
     position: relative;
 }
-form span{
+form span {
     display: inline-block;
     width: 100px;
     height: 30px;
 }
-form i{
+form i {
     display: inline-block;
     width: 32px;
     height: 32px;
@@ -251,13 +288,13 @@ form i{
     right: 35px;
     cursor: pointer;
 }
-i.invisible{
-    background: url('../../assets/invisible.png') no-repeat;
+i.invisible {
+    background: url("../../assets/invisible.png") no-repeat;
 }
-i.visible{
-    background: url('../../assets/visible.png') no-repeat;
+i.visible {
+    background: url("../../assets/visible.png") no-repeat;
 }
-input{
+input {
     width: 300px;
     height: 40px;
     text-indent: 10px;
@@ -266,29 +303,29 @@ input{
     border-radius: 3px;
     outline: none;
 }
-input.input-error{
+input.input-error {
     border-color: red;
 }
-input.input-error:focus{
+input.input-error:focus {
     border-color: red;
 }
-input:focus{
-    border: 1px solid #1E90FF;
+input:focus {
+    border: 1px solid #1e90ff;
 }
-div.change-lang{
+div.change-lang {
     margin: 30px 50px 0 0;
-    >span{
+    > span {
         margin-right: 10px;
         padding: 5px;
         color: #fff;
     }
-    >select{
+    > select {
         width: 120px;
         height: 30px;
         font-size: 14px;
         text-indent: 15px;
         border-radius: 5px;
-        &:focus{
+        &:focus {
             border-radius: 5px;
         }
     }
