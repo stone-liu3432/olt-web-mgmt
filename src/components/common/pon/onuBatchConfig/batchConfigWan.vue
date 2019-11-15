55f38dbdbb342d5cb473ef4e6d5c7ec489db8943
stone-liu3432 <liushuang3432@163.com>
2019-11-15 10:20:16 +0800

    onu wan连接配置集成至此页面

--- src/components/common/pon/onuBatchConfig/batchConfigWan.vue
+++ src/components/common/pon/onuBatchConfig/batchConfigWan.vue
@@ -15,18 +15,34 @@
                     <a
                         href="javascript: void(0);"
                         class="btn-text"
-                        @click="jumpToWanConfig(rows.port_id, rows.onu_id)"
+                        @click="openDialog(rows)"
                     >{{ lanMap['detail'] }}</a>
+                    <!-- <nms-dropdown :offset-y="30" @command="command">
+                        {{ lanMap['config'] }}
+                        <template slot="dropdown">
+                            <nms-dropdown-item :command="composeData('add', rows)">1</nms-dropdown-item>
+                            <nms-dropdown-item
+                                :command="composeData('detail', rows)"
+                                :disabled="rows.count === 0"
+                            >3</nms-dropdown-item>
+                        </template>
+                    </nms-dropdown> -->
                 </template>
             </nms-table-column>
         </nms-table>
+        <nms-dialog :visible.sync="visible">
+            <div slot="title">{{ lanMap['detail'] }}</div>
+            <onu-wan-config :port-data="portData" style="height: 600px;"></onu-wan-config>
+        </nms-dialog>
     </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import onuWanConfig from "./onuWanConfig";
 export default {
     name: "batchWan",
+    components: { onuWanConfig },
     computed: {
         ...mapState(["lanMap"])
     },
@@ -38,7 +54,9 @@ export default {
     },
     data() {
         return {
-            wanList: []
+            wanList: [],
+            visible: false,
+            portData: {}
         };
     },
     created() {
@@ -47,16 +65,19 @@ export default {
         }
     },
     // mounted() {
-    //     this.wanList = [
-    //         {
+    //     this.wanList = Array.from({ length: 30 }).map((item, index) => {
+    //         return {
     //             port_id: 1,
-    //             onu_id: 2,
+    //             onu_id: index + 1,
     //             macaddr: "38:3a:21:20:10:11",
     //             status: 1,
     //             count: 0,
-    //             wanmax: 2
-    //         }
-    //     ];
+    //             wanmax: 2,
+    //             lanports: 4,
+    //             wlan: 2,
+    //             voip: 2
+    //         };
+    //     });
     // },
     methods: {
         getData(port_id) {
@@ -72,15 +93,15 @@ export default {
                 })
                 .catch(err => {});
         },
-        jumpToWanConfig(port_id, onu_id) {
-            this.$router.push({
-                path: "/onu_basic_info",
-                query: {
-                    port_id,
-                    onu_id,
-                    flag: "wan"
-                }
-            });
+        openDialog(row) {
+            this.portData = {
+                portid: row.port_id,
+                onuid: row.onu_id,
+                geports: row.lanports,
+                wlan: row.wlan,
+                voip: row.voip
+            };
+            this.visible = true;
         }
     },
     watch: {
