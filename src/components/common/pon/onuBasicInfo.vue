4b8cbd2ccfb23e1c05457525d4854357b036b001
stone-liu3432 <liushuang3432@163.com>
2019-11-15 10:13:44 +0800

    去除wan连接配置和wlan配置

--- src/components/common/pon/onuBasicInfo.vue
+++ src/components/common/pon/onuBasicInfo.vue
@@ -72,8 +72,6 @@
             </div>
         </div>
         <onuAlarm v-if="show_page === 'onu_alarm'" :port-data="{ portid, onuid }" ref="onuAlarm"></onuAlarm>
-        <onu-wan-config v-if="show_page === 'wan_connect'" :port-data="{ portid, onuid }" ref="onuWan"></onu-wan-config>
-        <onu-wlan v-if="show_page === 'onu_wlan'" :port-data="{ portid, onuid }" ref="onuWlan"></onu-wlan>
         <div class="modal-dialog" v-if="onu_cfg_name">
             <div class="cover"></div>
             <div class="onu-desc">
@@ -107,28 +105,16 @@
 <script>
 import { mapState,mapMutations } from 'vuex'
 import onuAlarm from './onuAlarm';
-import onuWanConfig from './onuWanConfig';
-import onuWlan from './onuWlan';
     export default {
         name: 'onuBasicInfo',
         computed: {
             ...mapState(['lanMap','port_name','port_info','change_url','onu_list']),
             tabs(){
-                //return ['onu_info', 'onu_alarm', 'wan_connect', 'onu_wlan'];
                 let list = ['onu_info', 'onu_alarm'];
-                if(this.onu_basic_info.data && this.onu_basic_info.data.fw_ver){
-                    let fw_ver = this.onu_basic_info.data.fw_ver, reg = /^4853/;
-                    if(reg.test(fw_ver)){
-                        list.push('wan_connect');
-                        if(this.onu_basic_info.data.wlan){
-                            list.push('onu_wlan');
-                        }
-                    }
-                }
                 return list
             }
         },
-        components: { onuAlarm, onuWanConfig, onuWlan },
+        components: { onuAlarm },
         data(){
             return {
                 onu_basic_info: {},
@@ -153,13 +139,10 @@ import onuWlan from './onuWlan';
         },
         created(){
             var pid = Number(sessionStorage.getItem('pid'));
-            const { port_id, onu_id, flag } = this.$route.query;
+            const { port_id, onu_id } = this.$route.query;
             this.portid = Number(this.$route.query.port_id) || pid || 1;
             //  防止同时触发 created 和 activated
             this.isCreated = true;
-            if(flag){
-                flag === 'wan' && (this.show_page = 'wan_connect');
-            }
             if(this.change_url.beta === 'test'){
                 this.$http.get('./simulation_data/onu_resource.json').then(res=>{
                     if(res.data.code === 1){
@@ -195,14 +178,11 @@ import onuWlan from './onuWlan';
                 this.isCreated = false;
                 return;
             }
-            const { port_id, onu_id, flag } = this.$route.query;
+            const { port_id, onu_id } = this.$route.query;
             var pid = Number(sessionStorage.getItem('pid'));
             this.portid = Number(this.$route.query.port_id) || pid || 1;
             if(pid === this.portid){
                 this.get_resource();
-            }
-            if(flag){
-                flag === 'wan' && (this.show_page = 'wan_connect');
             }
         },
 		methods:{
@@ -505,14 +485,6 @@ import onuWlan from './onuWlan';
                             this.$nextTick(()=>{
                                 this.$refs.onuAlarm.getData();
                             })
-                        }else if(this.show_page === 'wan_connect'){
-                            this.$nextTick(_ =>{
-                                this.$refs.onuWan.getData();
-                            })
-                        }else if(this.show_page === 'onu_wlan'){
-                            this.$nextTick(_ => {
-                                this.$refs.onuWlan.getData();
-                            })
                         }
                     }else{
                         this.addonu_list({});
@@ -552,14 +524,6 @@ import onuWlan from './onuWlan';
                 }else if(this.show_page === 'onu_alarm'){
                     this.$nextTick(()=>{
                         this.$refs.onuAlarm.getData();
-                    })
-                }else if(this.show_page === 'wan_connect'){
-                    this.$nextTick(_ => {
-                        this.$refs.onuWan.getData();
-                    })
-                }else if(this.show_page === 'onu_wlan'){
-                    this.$nextTick(_ => {
-                        this.$refs.onuWlan.getData();
                     })
                 }
                 this.clearFile();
