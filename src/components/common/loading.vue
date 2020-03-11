cd09dae3d154aaa98aa10764eed0a5b4c9ab5b94
stone-liu3432 <liushuang3432@163.com>
2020-03-11 12:16:34 +0800

    修改loading的zindex值

--- src/components/common/loading.vue
+++ src/components/common/loading.vue
@@ -3,33 +3,111 @@
         <div class="loading-cover"></div>
         <div class="loading-content">
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 32 32"
+                    style="enable-background:new 0 0 50 50;"
+                    xml:space="preserve"
+                >
                     <circle cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(45 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.125s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.125s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(90 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.25s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.25s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(135 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.375s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.375s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(180 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.5s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(225 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.625s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.625s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(270 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.75s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.75s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(315 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.875s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.875s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                     <circle transform="rotate(180 16 16)" cx="16" cy="3" r="0">
-                        <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+                        <animate
+                            attributeName="r"
+                            values="0;3;0;0"
+                            dur="1s"
+                            repeatCount="indefinite"
+                            begin="0.5s"
+                            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                            calcMode="spline"
+                        />
                     </circle>
                 </svg>
             </div>
@@ -38,43 +116,43 @@
 </template>
 
 <script>
-    export default {
-        name: 'loading',
-        data(){
-            return {
-                loading: true
-            }
-        },
-        methods: {
-            close(){
-                this.loading = false;
-            }
+export default {
+    name: "loading",
+    data() {
+        return {
+            loading: true
+        };
+    },
+    methods: {
+        close() {
+            this.loading = false;
         }
     }
+};
 </script>
 
 <style scoped>
-div.loading{
+div.loading {
     width: 100%;
     height: 100%;
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 1000;
+    z-index: 99999;
 }
-div.loading-cover{
+div.loading-cover {
     width: 100%;
     height: 100%;
     position: absolute;
     background: #666;
     opacity: 0.5;
 }
-div.loading-content{
+div.loading-content {
     width: 100%;
     height: 100%;
     text-align: center;
 }
-div.loading-content>div{
+div.loading-content > div {
     position: relative;
     top: 200px;
     width: 120px;
@@ -84,11 +162,12 @@ div.loading-content>div{
     background: #555;
     border-radius: 10px;
 }
-svg{
+svg {
     width: 120px;
     height: 120px;
 }
-svg path,svg circle {
-    fill: #3990E5;
+svg path,
+svg circle {
+    fill: #3990e5;
 }
 </style>
