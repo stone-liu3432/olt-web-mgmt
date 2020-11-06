b92857e4a0cb5b46e83e87bf324bf5f707a063ff
stone-liu3432 <liushuang3432@163.com>
2020-11-06 15:38:14 +0800

    添加onu_id查找

--- src/components/common/switch/mac/query.vue
+++ src/components/common/switch/mac/query.vue
@@ -2,8 +2,13 @@
     <div class="query">
         <span>{{ lanMap["query_method"] }}</span>
         <select v-model.number="flag" key="flag">
-            <option :value="1">{{ lanMap["mac_type"] }}</option>
+            <template v-if="type !== 'pon'">
+                <option :value="1">{{ lanMap["mac_type"] }}</option>
+            </template>
             <option :value="2">{{ lanMap["port_id"] }}</option>
+            <template v-if="type === 'pon'">
+                <option :value="16">{{ lanMap["onu_id"] }}</option>
+            </template>
             <option :value="4">{{ lanMap["vlan_id"] }}</option>
             <option :value="8">{{ lanMap["macaddr"] }}</option>
         </select>
@@ -14,18 +19,26 @@
                 </template>
             </select>
         </template>
-        <template v-if="flag === 2">
+        <template v-if="flag === 2 || flag === 16">
             <select v-model.number="form.port_id" key="port-id">
                 <template v-for="item in port_name.pon">
                     <option :value="item.id">{{ item.name }}</option>
                 </template>
-                <template v-for="item in port_name.ge">
-                    <option :value="item.id">{{ item.name }}</option>
-                </template>
-                <template v-for="item in port_name.xge || {}">
-                    <option :value="item.id">{{ item.name }}</option>
+                <template v-if="type !== 'pon'">
+                    <template v-for="item in port_name.ge">
+                        <option :value="item.id">{{ item.name }}</option>
+                    </template>
+                    <template v-for="item in port_name.xge || {}">
+                        <option :value="item.id">{{ item.name }}</option>
+                    </template>
                 </template>
             </select>
+            <template v-if="flag === 16">
+                <span style="width: auto; margin: 0 12px">
+                    {{ lanMap["onu_id"] }}:
+                </span>
+                <input type="text" v-model.number="form.onu_id" key="onu-id" />
+            </template>
         </template>
         <template v-if="flag === 4">
             <input type="text" v-model="form.vlan_id" key="vlan-id" />
@@ -47,6 +60,9 @@ export default {
         data: {
             type: Array,
         },
+        type: {
+            type: String,
+        },
     },
     data() {
         return {
@@ -58,8 +74,12 @@ export default {
                 port_id: 1,
                 vlan_id: "",
                 macaddr: "",
+                onu_id: "",
             },
         };
+    },
+    created() {
+        this.type === "pon" && (this.flag = 2);
     },
     methods: {
         resetFields() {
@@ -68,6 +88,7 @@ export default {
                 port_id: 1,
                 vlan_id: "",
                 macaddr: "",
+                onu_id: "",
             };
         },
         computedData() {
@@ -108,6 +129,20 @@ export default {
                             (item) =>
                                 item.macaddr.indexOf(this.form.macaddr) > -1
                         )
+                    );
+                    break;
+                case 16:
+                    this.$emit(
+                        "data-change",
+                        this.macTable.filter((item) => {
+                            if (this.form.onu_id) {
+                                return (
+                                    item.port_id === this.form.port_id &&
+                                    item.onu_id === this.form.onu_id
+                                );
+                            }
+                            return item.port_id === this.form.port_id;
+                        })
                     );
                     break;
             }
