28a243e7e8dea44e5ab2e3d4c753a778c2de5c78
stone-liu3432 <liushuang3432@163.com>
2020-11-12 14:34:27 +0800

    update

--- src/components/common/commonComponent/pagination/pagination.vue
+++ src/components/common/commonComponent/pagination/pagination.vue
@@ -1,42 +1,74 @@
 <template>
     <ul class="pagination" v-if="!hide">
-        <li :class="index > 1 ? '' : 'disabled'" @click="changeIndex(1)" title="First">&lt;&lt;</li>
-        <li :class="index > 1 ? '' : 'disabled'" @click="changeIndex(index - 1)" title="Prev">&lt;</li>
-        <template v-if="(page - index + 3 <= pageCount) && page - 3 > 0">
+        <li
+            :class="index > 1 ? '' : 'disabled'"
+            @click="changeIndex(1)"
+            title="First"
+        >
+            &lt;&lt;
+        </li>
+        <li
+            :class="index > 1 ? '' : 'disabled'"
+            @click="changeIndex(index - 1)"
+            title="Prev"
+        >
+            &lt;
+        </li>
+        <template v-if="page - index + 3 <= pageCount && page - 3 > 0">
             <li
-                v-if="(index - 4 + pageCount > page) && index - 4 > 0"
-                @click="changeIndex(index  - 4)"
-            >{{ index - 4 }}</li>
+                v-if="index - 4 + pageCount > page && index - 4 > 0"
+                @click="changeIndex(index - 4)"
+            >
+                {{ index - 4 }}
+            </li>
             <li
-                v-if="(index - 3 + pageCount > page) && index - 3 > 0"
-                @click="changeIndex(index  - 3)"
-            >{{ index - 3 }}</li>
+                v-if="index - 3 + pageCount > page && index - 3 > 0"
+                @click="changeIndex(index - 3)"
+            >
+                {{ index - 3 }}
+            </li>
         </template>
-        <li v-if="index > 2" @click="changeIndex(index - 2)">{{ index - 2 }}</li>
-        <li v-if="index > 1" @click="changeIndex(index - 1)">{{ index - 1 }}</li>
+        <li v-if="index > 2" @click="changeIndex(index - 2)">
+            {{ index - 2 }}
+        </li>
+        <li v-if="index > 1" @click="changeIndex(index - 1)">
+            {{ index - 1 }}
+        </li>
         <li class="actived" @click="changeIndex(index)">{{ index }}</li>
-        <li v-if="page - 1 >= index" @click="changeIndex(index + 1)">{{ index + 1 }}</li>
-        <li v-if="page - 2 >= index" @click="changeIndex(index + 2)">{{ index + 2 }}</li>
-        <template v-if="(index + 3 <= pageCount) && page - 3 > 0">
+        <li v-if="page - 1 >= index" @click="changeIndex(index + 1)">
+            {{ index + 1 }}
+        </li>
+        <li v-if="page - 2 >= index" @click="changeIndex(index + 2)">
+            {{ index + 2 }}
+        </li>
+        <template v-if="index + 3 <= pageCount && page - 3 > 0">
             <li
-                v-if="(index + 3 <= pageCount) && index + 3 <= page"
+                v-if="index + 3 <= pageCount && index + 3 <= page"
                 @click="changeIndex(index + 3)"
-            >{{ index + 3 }}</li>
+            >
+                {{ index + 3 }}
+            </li>
             <li
-                v-if="(index + 4 <= pageCount) && index + 4 <= page"
+                v-if="index + 4 <= pageCount && index + 4 <= page"
                 @click="changeIndex(index + 4)"
-            >{{ index + 4 }}</li>
+            >
+                {{ index + 4 }}
+            </li>
         </template>
         <li
             :class="page - 1 >= index ? '' : 'disabled'"
             @click="changeIndex(index + 1)"
             title="Next"
-        >&gt;</li>
+        >
+            &gt;
+        </li>
         <li
             :class="page !== index ? '' : 'disabled'"
             @click="changeIndex(page)"
             title="End"
-        >&gt;&gt;</li>
+        >
+            &gt;&gt;
+        </li>
     </ul>
 </template>
 
@@ -47,7 +79,7 @@ export default {
         return {
             index: this.currentPage,
             // 显示的页码数
-            pageCount: 5
+            pageCount: 5,
         };
     },
     computed: {
@@ -56,29 +88,29 @@ export default {
         },
         hide() {
             return this.hideOnSinglePage && this.total <= this.pageSize;
-        }
+        },
     },
     props: {
         // 每页显示的条数
         pageSize: {
             type: Number,
-            default: 20
+            default: 20,
         },
         // 当前页
         currentPage: {
             type: Number,
-            default: 1
+            default: 1,
         },
         // 总条数
         total: {
             type: Number,
-            required: true
+            required: true,
         },
         // 单页(总条数少于每页条数)时是否隐藏分页
         hideOnSinglePage: {
             type: Boolean,
-            default: true
-        }
+            default: true,
+        },
     },
     methods: {
         changeIndex(index) {
@@ -87,13 +119,13 @@ export default {
             }
             this.index = index;
             this.$emit("current-change", index);
-        }
+        },
     },
     watch: {
         currentPage() {
             this.index = this.currentPage;
-        }
-    }
+        },
+    },
 };
 </script>
 
