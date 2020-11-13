80b905f9f58cb34e1d12bf50a52f9120b1496f12
stone-liu3432 <liushuang3432@163.com>
2020-11-13 14:04:42 +0800

    VLAN Translate从查看配置单个端口修改为所有端口

--- src/components/common/switch/portVlan/pvTranslate.vue
+++ src/components/common/switch/portVlan/pvTranslate.vue
@@ -1,36 +1,77 @@
 <template>
     <div class="port-vlan-translate">
-        <div class="pv-translate-item">
-            <span>Old VLAN</span>
-            <span>New VLAN</span>
-            <span>{{ lanMap['new_vlan_pri'] }}</span>
-            <span>
-                <a href="javascript:void(0);" @click="open_add_translate">{{ lanMap['add'] }}</a>
-            </span>
+        <h2>
+            <span>VLAN Translate</span>
+            <nms-button @click="open_add_translate">{{
+                lanMap["add"]
+            }}</nms-button>
+        </h2>
+        <div class="query-form">
+            <span>{{ lanMap["query_method"] }}:</span>
+            <select v-model.number="flag">
+                <option :value="1">{{ lanMap["port_id"] }}</option>
+                <option :value="2">Old VLAN</option>
+                <option :value="4">New VLAN</option>
+            </select>
+            <template v-if="flag === 1">
+                <select v-model.number="queryForm.port_id">
+                    <option :value="0">{{ lanMap["all"] }}</option>
+                    <template v-for="item in port_info.data || []">
+                        <option :value="item.port_id">
+                            {{ item.port_id | getPortName }}
+                        </option>
+                    </template>
+                </select>
+            </template>
+            <template v-if="flag === 2 || flag === 4">
+                <input type="text" v-model="queryForm.vlan_id" />
+            </template>
         </div>
-        <div
-            class="pv-translate-item"
-            v-for="(item,key) in pvData.data"
-            :key="key"
-            v-if="pvData.data && pvData.data.length > 0"
-        >
-            <span>{{ item.old_vlan }}</span>
-            <span>{{ item.new_vlan }}</span>
-            <span>{{ item.new_vlan_pri === 255 ? ' - ' : item.new_vlan_pri }}</span>
-            <span>
-                <!-- <a href="javascript:void(0);" @click="open_cfg_translate(item)">{{ lanMap['modify'] }}</a> -->
-                <a
-                    href="javascript:void(0);"
-                    @click="open_del_translate(item)"
-                >{{ lanMap['delete'] }}</a>
-            </span>
-        </div>
+        <nms-table :rows="vlanList" border>
+            <nms-table-column :label="lanMap['port_id']">
+                <template slot-scope="row">{{
+                    row.port_id | getPortName
+                }}</template>
+            </nms-table-column>
+            <nms-table-column
+                label="Old VLAN"
+                prop="old_vlan"
+            ></nms-table-column>
+            <nms-table-column
+                label="New VLAN"
+                prop="new_vlan"
+            ></nms-table-column>
+            <nms-table-column :label="lanMap['new_vlan_pri']">
+                <template slot-scope="row">{{
+                    row.new_vlan_pri === 255 ? "-" : row.new_vlan_pri
+                }}</template>
+            </nms-table-column>
+            <nms-table-column :label="lanMap['config']">
+                <template slot-scope="row">
+                    <nms-button type="text" @click="open_del_translate(row)">{{
+                        lanMap["delete"]
+                    }}</nms-button>
+                </template>
+            </nms-table-column>
+        </nms-table>
         <div class="modal-dialog" v-if="show_set_pvtrans">
             <div class="cover"></div>
             <div class="pv-translate-modal">
                 <div class="translate-modal-item">
-                    <h3 v-if="flags === 2">{{ lanMap['config'] }}</h3>
-                    <h3 v-if="flags === 1">{{ lanMap['add'] }}</h3>
+                    <h3 v-if="flags === 2">{{ lanMap["config"] }}</h3>
+                    <h3 v-if="flags === 1">{{ lanMap["add"] }}</h3>
+                </div>
+                <div class="translate-modal-item">
+                    <span>{{ lanMap["port_id"] }}</span>
+                    <span>
+                        <select v-model.number="port_id" style="width: 197px">
+                            <template v-for="item in port_info.data || []">
+                                <option :value="item.port_id">
+                                    {{ item.port_id | getPortName }}
+                                </option>
+                            </template>
+                        </select>
+                    </span>
                 </div>
                 <div class="translate-modal-item">
                     <span>Old VLAN</span>
@@ -39,7 +80,15 @@
                             type="text"
                             v-model.number="pv_old_vlan"
                             :disabled="flags === 2"
-                            :style="{ 'border-color' : this.pv_old_vlan !== '' && (this.pv_old_vlan < 1 || this.pv_old_vlan > 4094 || isNaN(this.pv_old_vlan)) ? 'red' : '' }"
+                            :style="{
+                                'border-color':
+                                    this.pv_old_vlan !== '' &&
+                                    (this.pv_old_vlan < 1 ||
+                                        this.pv_old_vlan > 4094 ||
+                                        isNaN(this.pv_old_vlan))
+                                        ? 'red'
+                                        : '',
+                            }"
                         />
                     </span>
                 </div>
@@ -49,32 +98,41 @@
                         <input
                             type="text"
                             v-model.number="pv_new_vlan"
-                            :style="{ 'border-color' : this.pv_new_vlan !== '' && (this.pv_new_vlan < 1 || this.pv_new_vlan > 4094 || isNaN(this.pv_new_vlan)) ? 'red' : '' }"
+                            :style="{
+                                'border-color':
+                                    this.pv_new_vlan !== '' &&
+                                    (this.pv_new_vlan < 1 ||
+                                        this.pv_new_vlan > 4094 ||
+                                        isNaN(this.pv_new_vlan))
+                                        ? 'red'
+                                        : '',
+                            }"
                         />
                     </span>
                 </div>
                 <div class="translate-modal-item">
-                    <span>{{ lanMap['new_vlan_pri'] }}</span>
+                    <span>{{ lanMap["new_vlan_pri"] }}</span>
                     <span>
                         <select v-model.number="pv_new_vlan_pri">
-                            <option value="255">-</option>
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
+                            <option :value="255">-</option>
+                            <option :value="0">0</option>
+                            <template v-for="i in 7">
+                                <option :value="i">{{ i }}</option>
+                            </template>
                         </select>
                     </span>
                 </div>
                 <div class="translate-modal-item">
-                    <a href="javascript:void(0);" @click="submit_pv_translate">{{ lanMap['apply'] }}</a>
+                    <a
+                        href="javascript:void(0);"
+                        @click="submit_pv_translate"
+                        >{{ lanMap["apply"] }}</a
+                    >
                     <a
                         href="javascript:void(0);"
                         @click="close_translate_modal"
-                    >{{ lanMap['cancel'] }}</a>
+                        >{{ lanMap["cancel"] }}</a
+                    >
                 </div>
                 <div class="close" @click="close_translate_modal"></div>
             </div>
@@ -86,31 +144,67 @@
 import { mapState } from "vuex";
 export default {
     name: "pvTranslate",
-    computed: mapState(["lanMap"]),
-    props: ["pvData"],
+    computed: {
+        ...mapState(["lanMap", "port_info"]),
+        vlanList() {
+            switch (this.flag) {
+                case 1:
+                    if (this.queryForm.port_id === 0) {
+                        return this.pvData;
+                    }
+                    return this.pvData.filter(
+                        (item) => item.port_id === this.queryForm.port_id
+                    );
+                    break;
+                case 2:
+                    return this.pvData.filter(
+                        (item) =>
+                            String(item.old_vlan).indexOf(
+                                this.queryForm.vlan_id
+                            ) > -1
+                    );
+                    break;
+                case 4:
+                    return this.pvData.filter(
+                        (item) =>
+                            String(item.new_vlan).indexOf(
+                                this.queryForm.vlan_id
+                            ) > -1
+                    );
+                    break;
+            }
+            return [];
+        },
+    },
+    props: {
+        pvData: {
+            type: Array,
+            default: () => [],
+        },
+    },
     data() {
         return {
             flags: 0, //  模态框内标题
-            cache_d: {},
             show_set_pvtrans: false,
+            port_id: 1,
             pv_old_vlan: 0,
             pv_new_vlan: 0,
-            pv_new_vlan_pri: 255
-            // pvData: {
-            //     "code":1,
-            //     "msg":"success",
-            //     "data":[{
-            //         "port_id":1,
-            //         "old_vlan":100,
-            //         "new_vlan":200,
-            //         "new_vlan_pri":1,
-            //         "action":0
-            //     }]
-            // }
+            pv_new_vlan_pri: 255,
+            flag: 1,
+            queryForm: {
+                port_id: 0,
+                vlan_id: "",
+            },
+            // pvData: [
+            //     {
+            //         port_id: 1,
+            //         old_vlan: 100,
+            //         new_vlan: 200,
+            //         new_vlan_pri: 1,
+            //         action: 0,
+            //     },
+            // ],
         };
-    },
-    created() {
-        //  to do
     },
     methods: {
         //  打开配置 translate 模态框
@@ -120,7 +214,6 @@ export default {
             this.pv_new_vlan = item.new_vlan;
             this.pv_old_vlan = item.old_vlan;
             this.pv_new_vlan_pri = item.new_vlan_pri;
-            this.cache_d = Object.assign({}, item);
         },
         //  关闭配置 translate 模态框
         close_translate_modal() {
@@ -132,25 +225,13 @@ export default {
         //  配置 translate 模态框内提交按钮
         submit_pv_translate() {
             if (
-                this.pv_old_vlan === this.cache_d.old_vlan &&
-                this.pv_new_vlan === this.cache_d.new_vlan &&
-                this.pv_new_vlan_pri === this.cache_d.new_vlan_pri
-            ) {
-                this.$message({
-                    type: "info",
-                    text: this.lanMap["modify_tips"]
-                });
-                return;
-            }
-            if (
                 this.pv_old_vlan < 1 ||
                 this.pv_old_vlan > 4094 ||
                 isNaN(this.pv_old_vlan)
             ) {
-                this.$message({
-                    type: "error",
-                    text: this.lanMap["param_error"] + ": " + "old vlan"
-                });
+                this.$message.error(
+                    this.lanMap["param_error"] + ": " + "old vlan"
+                );
                 return;
             }
             if (
@@ -158,82 +239,80 @@ export default {
                 this.pv_new_vlan > 4094 ||
                 isNaN(this.pv_new_vlan)
             ) {
-                this.$message({
-                    type: "error",
-                    text: this.lanMap["param_error"] + ": " + "new vlan"
-                });
+                this.$message.error(
+                    this.lanMap["param_error"] + ": " + "new vlan"
+                );
                 return;
+            }
+            if (
+                this.pvData.some(
+                    (item) =>
+                        item.port_id === this.port_id &&
+                        item.old_vlan === this.pv_old_vlan &&
+                        item.new_vlan === this.pv_new_vlan &&
+                        item.new_vlan_pri === this.pv_new_vlan_pri
+                )
+            ) {
+                return this.$message.info(this.lanMap["translate_exits"]);
             }
             var post_params = {
                 method: "set",
                 param: {
-                    port_id: this.$parent.$data.portid,
+                    port_id: this.port_id,
                     old_vlan: this.pv_old_vlan,
                     new_vlan: this.pv_new_vlan,
                     new_vlan_pri: this.pv_new_vlan_pri,
-                    action: 0
-                }
+                    action: 0,
+                },
             };
             this.$http
                 .post("/vlan_action?form=translate", post_params)
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
-                        this.$message({
-                            type: res.data.type,
-                            text: this.lanMap["setting_ok"]
-                        });
-                        this.$parent.get_pv_translate();
-                    } else if (res.data.code > 1) {
-                        this.$message({
-                            type: res.data.type,
-                            text: "(" + res.data.code + ") " + res.data.message
-                        });
+                        this.$message.success(this.lanMap["setting_ok"]);
+                        this.$emit("refresh-data");
+                    } else {
+                        this.$message.error(
+                            "(" + res.data.code + ") " + res.data.message
+                        );
                     }
                 })
-                .catch(err => {
-                    // to do
-                });
+                .catch((err) => {});
             this.close_translate_modal();
         },
         //  打开删除 translate 确认框
-        open_del_translate(item) {
-            this.$confirm(this.lanMap["if_sure"])
-                .then(_ => {
-                    this.del_pv_trans(item.old_vlan);
+        open_del_translate(row) {
+            this.$confirm(this.lanMap["if_sure"] + this.lanMap["delete"] + " ?")
+                .then((_) => {
+                    this.del_pv_trans(row);
                 })
-                .catch(_ => {});
+                .catch((_) => {});
         },
         //  确认框内的操作按钮
-        del_pv_trans(old_vlan) {
+        del_pv_trans(row) {
             const post_params = {
                 method: "delete",
                 param: {
-                    port_id: this.$parent.$data.portid,
-                    old_vlan,
-                    action: 0
-                }
+                    port_id: row.port_id,
+                    old_vlan: row.old_vlan,
+                    action: 0,
+                },
             };
             this.$http
                 .post("/vlan_action?form=translate", post_params)
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
-                        this.$message({
-                            type: res.data.type,
-                            text:
-                                this.lanMap["delete"] +
-                                this.lanMap["se_success"]
-                        });
-                        this.$parent.get_pv_translate();
-                    } else if (res.data.code > 1) {
-                        this.$message({
-                            type: res.data.type,
-                            text: "(" + res.data.code + ") " + res.data.message
-                        });
+                        this.$message.success(
+                            this.lanMap["delete"] + this.lanMap["st_success"]
+                        );
+                        this.$emit("refresh-data");
+                    } else {
+                        this.$message.error(
+                            "(" + res.data.code + ") " + res.data.message
+                        );
                     }
                 })
-                .catch(err => {
-                    //  to do
-                });
+                .catch((err) => {});
         },
         //  打开添加 translate 模态框
         open_add_translate() {
@@ -242,17 +321,37 @@ export default {
             this.pv_new_vlan = "";
             this.pv_old_vlan = "";
             this.pv_new_vlan_pri = 255;
-        }
-    }
+        },
+        resetQueryForm() {
+            this.queryForm = {
+                port_id: 0,
+                vlan_id: "",
+            };
+        },
+    },
+    watch: {
+        flag() {
+            this.resetQueryForm();
+        },
+    },
 };
 </script>
 
 <style lang="less" scoped>
-a {
-    font-size: 16px;
-    font-weight: normal;
-    width: 120px;
-    padding: 0;
+// a {
+//     font-size: 16px;
+//     font-weight: normal;
+//     width: 120px;
+//     padding: 0;
+// }
+h2 {
+    padding: 0 0 0 10px;
+    font-size: 18px;
+    font-weight: 600;
+    color: @titleColor;
+    a {
+        margin-left: 30px;
+    }
 }
 select {
     width: 120px;
@@ -288,7 +387,7 @@ div.pv-translate-item {
 div.modal-dialog {
     div.pv-translate-modal {
         width: 500px;
-        height: 300px;
+        height: 340px;
         background: #fff;
         div.translate-modal-item {
             margin: 20px 0 20px 60px;
@@ -316,6 +415,24 @@ div.modal-dialog {
                 margin: 10px 0 0 45px;
             }
         }
+    }
+}
+div.query-form {
+    margin: 20px 0 0 10px;
+    > span {
+        margin-right: 12px;
+    }
+    > select {
+        width: 150px;
+        & + select {
+            margin-left: 20px;
+        }
+    }
+    > input {
+        height: 30px;
+        box-sizing: border-box;
+        margin-left: 20px;
+        vertical-align: middle;
     }
 }
 </style>
