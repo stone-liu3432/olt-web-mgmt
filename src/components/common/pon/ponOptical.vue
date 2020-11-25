ea00280ca6dc9f657cf76ac3120ae4e578861612
stone-liu3432 <liushuang3432@163.com>
2020-11-25 15:37:59 +0800

    修复参数类型错误的问题，修改pon下onu光功率信息列表的显示

--- src/components/common/pon/ponOptical.vue
+++ src/components/common/pon/ponOptical.vue
@@ -1,36 +1,48 @@
 <template>
     <div class="pon-optical">
         <div class="pon-optical-title">
-            <h2>{{ lanMap['pon_optical'] }}</h2>
+            <h2>{{ lanMap["pon_optical"] }}</h2>
             <div v-if="show_index !== 'optical_module'">
-                <span>{{ lanMap['port_id'] }}</span>
+                <span>{{ lanMap["port_id"] }}</span>
                 <select v-model.number="portid">
                     <option
-                        v-for="(item,key) in port_name.pon"
+                        v-for="(item, key) in port_name.pon"
                         :key="key"
                         :value="item.id"
-                    >{{ item.name }}</option>
+                    >
+                        {{ item.name }}
+                    </option>
                 </select>
             </div>
-            <div v-if="is_show_onu && onu_list.data && show_index === 'pon_optical'">
-                <span>{{ lanMap['onu_id'] }}</span>
+            <div
+                v-if="
+                    is_show_onu && onu_list.data && show_index === 'pon_optical'
+                "
+            >
+                <span>{{ lanMap["onu_id"] }}</span>
                 <select v-model.number="onuid">
                     <option
-                        v-for="(item,index) in onu_list.data"
+                        v-for="(item, index) in onu_list.data"
                         :key="index"
                         :value="item"
-                    >{{ 'ONU'+ portid + '/' + item }}</option>
+                    >
+                        {{ "ONU" + portid + "/" + item }}
+                    </option>
                 </select>
             </div>
-            <div v-if="is_show_onu && !onu_list.data" class="error-msg">{{ lanMap['no_onu_info'] }}</div>
+            <div v-if="is_show_onu && !onu_list.data" class="error-msg">
+                {{ lanMap["no_onu_info"] }}
+            </div>
             <div v-if="!is_show_onu">
-                <a href="javascript:void(0);" @click="get_all_optical">{{ lanMap['refresh'] }}</a>
+                <a href="javascript:void(0);" @click="get_all_optical">{{
+                    lanMap["refresh"]
+                }}</a>
             </div>
         </div>
         <hr />
         <!--  v-if="onu_list.data && onu_list.data.length > 0" -->
         <tabBar
-            :tab="['optical_module' ,'pon_optical','onu_optical_diagnose']"
+            :tab="['optical_module', 'pon_optical', 'onu_optical_diagnose']"
             @togglePage="select_page"
         ></tabBar>
         <!-- RSSI -->
@@ -38,66 +50,91 @@
             href="javascript:void(0);"
             class="rt"
             @click="reload"
-            v-if="show_index === 'pon_optical' && pon_optical.data && onu_list.data && onu_list.data.length > 0"
-        >{{ lanMap['refresh'] }}</a>
+            v-if="
+                show_index === 'pon_optical' &&
+                pon_optical.data &&
+                onu_list.data &&
+                onu_list.data.length > 0
+            "
+            >{{ lanMap["refresh"] }}</a
+        >
         <div v-if="show_index === 'optical_module'">
             <optical-module :data="optical_module_info.data"></optical-module>
         </div>
         <p
-            v-if="show_index === 'pon_optical' && pon_optical.data && onu_list.data && onu_list.data.length > 0"
-        >{{ lanMap['pon_optical_tips'] }}</p>
+            v-if="
+                show_index === 'pon_optical' &&
+                pon_optical.data &&
+                onu_list.data &&
+                onu_list.data.length > 0
+            "
+        >
+            {{ lanMap["pon_optical_tips"] }}
+        </p>
         <ul
             class="rssi"
-            v-if="show_index === 'pon_optical' && pon_optical.data && onu_list.data && onu_list.data.length > 0"
+            v-if="
+                show_index === 'pon_optical' &&
+                pon_optical.data &&
+                onu_list.data &&
+                onu_list.data.length > 0
+            "
         >
-            <li v-for="(item,key) in pon_optical.data" :key="key">
+            <li v-for="(item, key) in pon_optical.data" :key="key">
                 <span>{{ lanMap[key] }}</span>
                 <span>{{ item }}</span>
             </li>
         </ul>
         <!-- 所有ONU的光模块诊断信息 -->
-        <ul
-            class="onu-optical"
-            v-if="show_index === 'onu_optical_diagnose' && all_onu_optical.data && all_onu_optical.data.length > 0"
-        >
-            <li class="bg-title">
-                <span>{{ lanMap['onu_id'] }}</span>
-                <span>{{ lanMap['work_temprature'] }}</span>
-                <span>{{ lanMap['work_voltage'] }}</span>
-                <span>{{ lanMap['transmit_bias'] }}</span>
-                <span>{{ lanMap['transmit_power'] }}</span>
-                <span>{{ lanMap['receive_power'] }}</span>
-            </li>
-            <li v-for="(item,key) in all_onu_optical.data" :key="key">
-                <span
-                    :title="item.onu_name"
-                >{{ item.onu_name ? item.onu_name : 'PON0' + item.port_id + '/' + item.onu_id }}</span>
-                <span>{{ item.work_temprature }}</span>
-                <span>{{ item.work_voltage }}</span>
-                <span>{{ item.transmit_bias }}</span>
-                <span>{{ item.transmit_power }}</span>
-                <span>{{ item.receive_power }}</span>
-            </li>
-        </ul>
-        <p
-            class="error-tips"
-            v-if="show_index === 'onu_optical_diagnose' && !all_onu_optical.data"
-        >{{ lanMap['onu_optical_tips'] }}</p>
-        <div
-            class="error-tips"
-            v-if="timeout_tips && show_index === 'onu_optical_diagnose'"
-        >{{ lanMap['get_data_fail'] }}</div>
-        <div class="error-tips" v-if="is_load">{{ lanMap['loading'] }}</div>
+        <template v-if="show_index === 'onu_optical_diagnose'">
+            <p>{{ lanMap["onu_optical_tips"] }}</p>
+            <nms-table :rows="all_onu_optical" border>
+                <nms-table-column :label="lanMap['onu_id']">
+                    <template slot-scope="row">
+                        {{ `${row.port_id} / ${row.onu_id}` }}
+                    </template>
+                </nms-table-column>
+                <nms-table-column
+                    :label="lanMap['onu_name']"
+                    prop="onu_name"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['macaddr']"
+                    prop="macaddr"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['work_temprature']"
+                    prop="work_temprature"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['work_voltage']"
+                    prop="work_voltage"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['transmit_bias']"
+                    prop="transmit_bias"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['transmit_power']"
+                    prop="transmit_power"
+                ></nms-table-column>
+                <nms-table-column
+                    :label="lanMap['receive_power']"
+                    prop="receive_power"
+                ></nms-table-column>
+            </nms-table>
+        </template>
     </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
+import { isArray } from "@/utils/common";
 import opticalModule from "./opticalModule";
 export default {
     name: "ponOptical",
     computed: {
-        ...mapState(["lanMap", "port_name", "change_url", "onu_list"])
+        ...mapState(["lanMap", "port_name", "change_url", "onu_list"]),
     },
     components: { opticalModule },
     data() {
@@ -118,20 +155,20 @@ export default {
                 //     "receive_power":"-3.3003 dBm"
                 // }
             },
-            all_onu_optical: {
-                // "code":1,
-                // "message":"success",
-                // "data":[{
-                //     "port_id":1,
-                //     "onu_id":1,
-                //     "onu_name":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-                //     "work_temprature":"21 C",
-                //     "work_voltage":"3.29 V",
-                //     "transmit_bias":"13 mA",
-                //     "transmit_power":"1.8295 dBm",
-                //     "receive_power":"-3.3003 dBm"
-                // }]
-            },
+            all_onu_optical: [
+                // {
+                //     port_id: 1,
+                //     onu_id: 1,
+                //     onu_name:
+                //         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                //     macaddr: "00:00:00:00:00:00",
+                //     work_temprature: "21 C",
+                //     work_voltage: "3.29 V",
+                //     transmit_bias: "13 mA",
+                //     transmit_power: "1.8295 dBm",
+                //     receive_power: "-3.3003 dBm",
+                // },
+            ],
             optical_module_info: {
                 // "code": 1,
                 // "message":"success",
@@ -147,8 +184,6 @@ export default {
             },
             show_index: "optical_module",
             is_show_onu: false,
-            timeout_tips: false,
-            is_load: false
         };
     },
     created() {
@@ -158,44 +193,43 @@ export default {
     },
     methods: {
         ...mapMutations({
-            addonu_list: "updateOnuList"
+            addonu_list: "updateOnuList",
         }),
         //  RSSI
         getData() {
             this.$http
                 .get("/ponmgmt?form=optical_pon", {
-                    params: { port_id: this.portid, onu_id: this.onuid }
+                    params: { port_id: this.portid, onu_id: this.onuid },
                 })
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         this.pon_optical = res.data;
                     } else {
                         this.pon_optical = {};
                     }
                 })
-                .catch(err => {
-                    // to do
-                });
+                .catch((err) => {});
         },
         //  获取所有ONU的光模块诊断信息
         get_all_optical() {
-            this.all_onu_optical = {};
-            this.timeout_tips = false;
-            this.is_load = true;
+            this.all_onu_optical = [];
+            const loading = this.$loading();
             this.$http
                 .get("/ponmgmt?form=optical_onu", {
-                    params: { port_id: this.portid }
+                    params: { port_id: this.portid },
                 })
-                .then(res => {
-                    this.is_load = false;
+                .then((res) => {
                     if (res.data.code === 1) {
-                        this.all_onu_optical = res.data;
+                        if (isArray(res.data.data)) {
+                            this.all_onu_optical = res.data.data;
+                        }
                     }
                 })
-                .catch(err => {
-                    // to do
-                    this.timeout_tips = true;
-                    this.is_load = false;
+                .catch((err) => {
+                    this.$message.warning(this.lanMap["get_data_fail"]);
+                })
+                .finally(() => {
+                    loading && loading.close();
                 });
         },
         //  分页切换
@@ -206,7 +240,6 @@ export default {
                 this.is_show_onu = false;
             } else if (page === "pon_optical") {
                 this.get_resource();
-                this.timeout_tips = false;
             } else if (page === "optical_module") {
                 this.is_show_onu = false;
                 this.get_optical_info();
@@ -238,13 +271,13 @@ export default {
                     result.push(Number(substrs));
                 }
             }
-            return result.filter(item => !!item);
+            return result.filter((item) => !!item);
         },
         //  获取PON口下的ONU列表
         get_resource() {
             this.$http
                 .get("/onu_allow_list?form=resource&port_id=" + this.portid)
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         var _onu_list = this.analysis(res.data.data.resource);
                         if (!_onu_list) {
@@ -255,7 +288,7 @@ export default {
                         }
                         var obj = {
                             port_id: this.portid,
-                            data: _onu_list
+                            data: _onu_list,
                         };
                         this.addonu_list(obj);
                         var oid = Number(sessionStorage.getItem("oid"));
@@ -273,7 +306,7 @@ export default {
                         this.onuid = 0;
                     }
                 })
-                .catch(err => {
+                .catch((err) => {
                     // to do
                 });
         },
@@ -281,13 +314,13 @@ export default {
             this.optical_module_info = {};
             this.$http
                 .get("/ponmgmt?form=optical_poninfo")
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         this.optical_module_info = res.data;
                     }
                 })
-                .catch(err => {});
-        }
+                .catch((err) => {});
+        },
     },
     watch: {
         portid() {
@@ -303,8 +336,8 @@ export default {
             if (!this.onuid) return;
             sessionStorage.setItem("oid", Number(this.onuid));
             this.getData();
-        }
-    }
+        },
+    },
 };
 </script>
 
