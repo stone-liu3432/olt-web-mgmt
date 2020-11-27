9ede87c2575d9855d84e1ed9ac9967a9d65354e8
stone-liu3432 <liushuang3432@163.com>
2020-11-27 11:06:05 +0800

    修复菜单过多时，显示不完全的问题

--- src/components/page/aside.vue
+++ src/components/page/aside.vue
@@ -2,16 +2,38 @@
     <div id="left-aside" class="lf">
         <ul class="menu" v-if="menu.data && lanMap">
             <!-- 主菜单/左侧导航栏 -->
-            <li v-for="(item,index) in menu.data.menu" :key="index">
-                <p class="menu-item" @click="select_first_menu(item)" :class="[ adv_f_menu === item.name ? 'active' : '' ]"> 
+            <li v-for="(item, index) in menu.data.menu" :key="index">
+                <p
+                    class="menu-item"
+                    @click="select_first_menu(item)"
+                    :class="[adv_f_menu === item.name ? 'active' : '']"
+                >
                     {{ lanMap[item.name] || item.name }}
                     <i class="icon-arrows" v-if="item.children"></i>
                 </p>
                 <!-- 二级菜单 -->
                 <transition name="bounce">
-                    <ul v-if="item.children" :class="['sub-menu', { 'hide': adv_f_menu === item.name }]">
-                        <li v-for="(_item,_index) in item.children" :key="_index">
-                            <p :class="['sub-item', {'submenu-actived': adv_menu === _item.name }]" @click="select_page(_item)">
+                    <ul
+                        v-if="item.children"
+                        :class="[
+                            'sub-menu',
+                            { hide: adv_f_menu === item.name },
+                        ]"
+                    >
+                        <li
+                            v-for="(_item, _index) in item.children"
+                            :key="_index"
+                        >
+                            <p
+                                :class="[
+                                    'sub-item',
+                                    {
+                                        'submenu-actived':
+                                            adv_menu === _item.name,
+                                    },
+                                ]"
+                                @click="select_page(_item)"
+                            >
                                 {{ lanMap[_item.name] || _item.name }}
                             </p>
                         </li>
@@ -21,19 +43,19 @@
         </ul>
         <div class="menu-footer">
             <div v-if="company_name">
-                <h4>{{ lanMap['company_name'] }}</h4>
+                <h4>{{ lanMap["company_name"] }}</h4>
                 <p>{{ company_name }}</p>
             </div>
             <div v-if="company_addr">
-                <h4>{{ lanMap['company_addr'] }}</h4>
+                <h4>{{ lanMap["company_addr"] }}</h4>
                 <p>{{ company_addr }}</p>
             </div>
             <div v-if="company_email">
-                <h4>{{ lanMap['company_email'] }}</h4>
+                <h4>{{ lanMap["company_email"] }}</h4>
                 <p>{{ company_email }}</p>
             </div>
             <div v-if="company_phone">
-                <h4>{{ lanMap['company_phone'] }}</h4>
+                <h4>{{ lanMap["company_phone"] }}</h4>
                 <p>{{ company_phone }}</p>
             </div>
         </div>
@@ -41,74 +63,81 @@
 </template>
 
 <script>
-import { mapState,mapMutations } from 'vuex'
+import { mapState, mapMutations } from "vuex";
 export default {
-    name: 'leftAside',
-    data(){
+    name: "leftAside",
+    data() {
         return {
-            company_name: '',
-            company_addr: '',
-            company_email: '',
-            company_phone: ''
-        }
+            company_name: "",
+            company_addr: "",
+            company_email: "",
+            company_phone: "",
+        };
     },
-    created(){
-        
+    created() {},
+    beforeDestroy() {
+        this.changeFMenu("running_status");
     },
-    beforeDestroy(){
-        this.changeFMenu('running_status');
-    },
-    methods:{
+    methods: {
         ...mapMutations({
-            addmenu: 'updateMenu',
-            changeAdvMenu: 'updateAdvMenu',
-            changeFMenu: 'updateAdvFMenu'
+            addmenu: "updateMenu",
+            changeAdvMenu: "updateAdvMenu",
+            changeFMenu: "updateAdvFMenu",
         }),
-        getData(){
-            this.$http.get('/board?info=setting_board').then(res=>{
-                if(res.data.code === 1){
-                    var data = res.data.data;
-                    this.company_name = data.name || '';
-                    this.company_addr = data.addr || '';
-                    this.company_email = data.email || '';
-                    this.company_phone = data.phone || '';
-                }else{
-                    this.company_name = '';
-                    this.company_addr = '';
-                    this.company_email = '';
-                    this.company_phone = '';
-                }
-            }).catch(err=>{
-                // to do
-            })
+        getData() {
+            this.$http
+                .get("/board?info=setting_board")
+                .then((res) => {
+                    if (res.data.code === 1) {
+                        var data = res.data.data;
+                        this.company_name = data.name || "";
+                        this.company_addr = data.addr || "";
+                        this.company_email = data.email || "";
+                        this.company_phone = data.phone || "";
+                    } else {
+                        this.company_name = "";
+                        this.company_addr = "";
+                        this.company_email = "";
+                        this.company_phone = "";
+                    }
+                })
+                .catch((err) => {
+                    // to do
+                });
         },
-        select_first_menu(node){
+        select_first_menu(node) {
             this.changeFMenu(node.name);
-            if(!node.children){
+            if (!node.children) {
                 this.$router.replace(node.name);
             }
         },
         //  点击切换页面
-        select_page(node){
+        select_page(node) {
             this.$router.replace(node.name);
-        }
+        },
     },
-    computed: mapState(['lanMap', 'menu', 'change_url', 'adv_menu', 'adv_f_menu']),
+    computed: mapState([
+        "lanMap",
+        "menu",
+        "change_url",
+        "adv_menu",
+        "adv_f_menu",
+    ]),
     watch: {
         //  页面刷新时的menu状态恢复
-        'menu'(){
-            var first_menu = sessionStorage.getItem('first_menu');
-            var sec_menu = sessionStorage.getItem('sec_menu');
+        menu() {
+            var first_menu = sessionStorage.getItem("first_menu");
+            var sec_menu = sessionStorage.getItem("sec_menu");
             this.changeFMenu(first_menu);
             this.changeAdvMenu(sec_menu);
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <style scoped lang="less">
-#left-aside{
-    width: 220px;;
+#left-aside {
+    width: 220px;
     height: calc(~"100% - 70px");
     overflow: auto;
     position: absolute;
@@ -118,84 +147,84 @@ export default {
     z-index: 99;
     user-select: none;
 }
-.menu>li>ul{
+.menu > li > ul {
     transition: all 0.2s ease-out;
 }
-.active{
+.active {
     border-left: 5px solid @activedAsideLeftBorderColor;
-    color:@activedAsideColor;
+    color: @activedAsideColor;
     background: @activedAsideBgColor;
-    i.icon-arrows{
+    i.icon-arrows {
         background-position: -48px -6px;
     }
 }
-.sub-menu>li>p.submenu-actived{
+.sub-menu > li > p.submenu-actived {
     background: @activedSubAsideBgColor;
     color: @activedSubAsideColor;
     font-weight: 500;
 }
-.menu>li>ul.hide{
-    max-height: 330px;
+.menu > li > ul.hide {
+    max-height: 350px;
     transition: max-height 0.3s ease-in;
 }
-.menu>li{
+.menu > li {
     overflow: hidden;
 }
-.menu-item{
-    padding:15px 0 15px 30px;
+.menu-item {
+    padding: 15px 0 15px 30px;
     cursor: pointer;
-    transition:all 0.2s linear;
+    transition: all 0.2s linear;
     height: 32px;
     line-height: 32px;
     position: relative;
     font-weight: 500;
 }
-.menu-item:hover{
+.menu-item:hover {
     background: @hoverAsideBgColor;
-} 
-.menu-footer{
+}
+.menu-footer {
     color: @asideFooterBgColor;
     font-size: 14px;
-    text-align:center;
+    text-align: center;
     margin: 60px auto 100px;
 }
-.sub-menu{
+.sub-menu {
     max-height: 0;
     height: auto;
 }
-.sub-item{
-    padding:10px 0 10px 40px;
+.sub-item {
+    padding: 10px 0 10px 40px;
     cursor: pointer;
     background: @subAsideBgColor;
     transition: all 0.2s linear;
     border-left: 5px solid transparent;
 }
-.sub-item:hover{
+.sub-item:hover {
     background: @hoverAsideBgColor;
 }
-.menu-footer{
+.menu-footer {
     color: @asideFooterColor;
-    div{
+    div {
         margin: 20px 0;
     }
-    h4{
+    h4 {
         text-align: left;
         padding-left: 10px;
         margin-bottom: 10px;
     }
-    p{
+    p {
         padding-left: 15px;
         word-wrap: break-word;
         word-break: break-all;
         text-align: left;
     }
 }
-i.icon-arrows{
+i.icon-arrows {
     position: absolute;
     right: 10px;
     width: 32px;
     height: 32px;
-    background: url('../../assets/arrows.png') -7px -48px no-repeat;
+    background: url("../../assets/arrows.png") -7px -48px no-repeat;
     vertical-align: middle;
 }
 </style>
