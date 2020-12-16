51d05af081af6700377438608423469780997d7e
stone-liu3432 <liushuang3432@163.com>
2020-12-16 17:12:31 +0800

    lease_time显示修改为lease_time_string

--- src/components/common/protocol/dhcp/snooping.vue
+++ src/components/common/protocol/dhcp/snooping.vue
@@ -1,81 +1,132 @@
 <template>
     <div>
         <div class="dhcp-snooping-item">
-            <span>{{ lanMap['snooping_admin'] }}</span>
-            <span>{{ admin === 1 ? lanMap['enable'] : lanMap['disable'] }}</span>
-            <a href="javascript: void(0);" @click="openDialog('admin')">{{ lanMap['config'] }}</a>
+            <span>{{ lanMap["snooping_admin"] }}</span>
+            <span>
+                {{ admin === 1 ? lanMap["enable"] : lanMap["disable"] }}
+            </span>
+            <a href="javascript: void(0);" @click="openDialog('admin')">
+                {{ lanMap["config"] }}
+            </a>
         </div>
         <div v-if="admin === 1">
             <div class="dhcp-snooping-item">
-                <span>{{ lanMap['chaddr_check'] }}</span>
-                <span>{{ check === 1 ? lanMap['enable'] : lanMap['disable'] }}</span>
-                <a href="javascript: void(0);" @click="openDialog('check')">{{ lanMap['config'] }}</a>
+                <span>{{ lanMap["chaddr_check"] }}</span>
+                <span>
+                    {{ check === 1 ? lanMap["enable"] : lanMap["disable"] }}
+                </span>
+                <a href="javascript: void(0);" @click="openDialog('check')">
+                    {{ lanMap["config"] }}
+                </a>
             </div>
             <div class="dhcp-snooping-item">
-                <span>{{ lanMap['response_time'] }}</span>
+                <span>{{ lanMap["response_time"] }}</span>
                 <span>{{ time }} s</span>
-                <a href="javascript: void(0);" @click="openDialog('time')">{{ lanMap['config'] }}</a>
+                <a href="javascript: void(0);" @click="openDialog('time')">
+                    {{ lanMap["config"] }}
+                </a>
             </div>
             <div class="dhcp-snooping-item">
-                <span>{{ lanMap['trust_portlist'] }}</span>
+                <span>{{ lanMap["trust_portlist"] }}</span>
                 <span>{{ portlist | parsePortList }}</span>
-                <a href="javascript: void(0);" @click="openDialog('portlist')">{{ lanMap['add'] }}</a>
+                <a href="javascript: void(0);" @click="openDialog('portlist')">
+                    {{ lanMap["add"] }}
+                </a>
                 <a
                     href="javascript: void(0);"
                     @click="openDialog('portlist', 'delete')"
-                >{{ lanMap['delete'] }}</a>
+                >
+                    {{ lanMap["delete"] }}
+                </a>
             </div>
             <div class="dhcp-snooping-item table-title">
-                <span>{{ lanMap['snooping_table'] }}</span>
-                <a href="javascript: void(0);" @click="clearSnoopingTable">{{ lanMap['clear'] }}</a>
-                <a href="javascript: void(0);" @click="refreshData">{{ lanMap['refresh'] }}</a>
+                <span>{{ lanMap["snooping_table"] }}</span>
+                <a href="javascript: void(0);" @click="clearSnoopingTable">
+                    {{ lanMap["clear"] }}
+                </a>
+                <a href="javascript: void(0);" @click="refreshData">
+                    {{ lanMap["refresh"] }}
+                </a>
             </div>
             <nms-table :rows="snoopingTable" border>
-                <nms-table-column prop="ipaddr" :label="lanMap['ipaddr']"></nms-table-column>
-                <nms-table-column prop="macaddr" :label="lanMap['macaddr']"></nms-table-column>
+                <nms-table-column
+                    prop="ipaddr"
+                    :label="lanMap['ipaddr']"
+                ></nms-table-column>
+                <nms-table-column
+                    prop="macaddr"
+                    :label="lanMap['macaddr']"
+                ></nms-table-column>
                 <nms-table-column prop="port_id" :label="lanMap['port_id']">
-                    <template slot-scope="rows">{{ rows.port_id | getPortName }}</template>
+                    <template slot-scope="rows">
+                        {{ rows.port_id | getPortName }}
+                    </template>
                 </nms-table-column>
-                <nms-table-column prop="vlan_id" :label="lanMap['vlan_id']"></nms-table-column>
-                <nms-table-column prop="lease_time" :label="lanMap['lease_time']"></nms-table-column>
-                <nms-table-column prop="entry_status" :label="lanMap['entry_status']">
-                    <template slot-scope="rows">{{ rows.entry_status ? 'Valid' : 'Invalid' }}</template>
+                <nms-table-column
+                    prop="vlan_id"
+                    :label="lanMap['vlan_id']"
+                ></nms-table-column>
+                <nms-table-column
+                    prop="lease_time_string"
+                    :label="lanMap['lease_time']"
+                ></nms-table-column>
+                <nms-table-column
+                    prop="entry_status"
+                    :label="lanMap['entry_status']"
+                >
+                    <template slot-scope="rows">
+                        {{ rows.entry_status ? "Valid" : "Invalid" }}
+                    </template>
                 </nms-table-column>
             </nms-table>
         </div>
-        <nms-dialog :visible.sync="visible" :width="dialogType === 'portlist' ? '700px' : '500px'">
-            <div
-                slot="title"
-            >{{ dialogType === 'portlist' ? dialogMethod === 'delete' ? lanMap['delete'] : lanMap['add'] :lanMap['set'] }}</div>
+        <nms-dialog
+            :visible.sync="visible"
+            :width="dialogType === 'portlist' ? '700px' : '500px'"
+        >
+            <div slot="title">
+                {{
+                    dialogType === "portlist"
+                        ? dialogMethod === "delete"
+                            ? lanMap["delete"]
+                            : lanMap["add"]
+                        : lanMap["set"]
+                }}
+            </div>
             <div v-if="dialogType === 'admin'" class="dhcp-dialog-content">
-                <span>{{ lanMap['snooping_admin'] }}</span>
+                <span>{{ lanMap["snooping_admin"] }}</span>
                 <select v-model.number="snooping_admin">
-                    <option :value="0">{{ lanMap['disable'] }}</option>
-                    <option :value="1">{{ lanMap['enable'] }}</option>
+                    <option :value="0">{{ lanMap["disable"] }}</option>
+                    <option :value="1">{{ lanMap["enable"] }}</option>
                 </select>
             </div>
             <div v-if="dialogType === 'check'" class="dhcp-dialog-content">
-                <span>{{ lanMap['chaddr_check'] }}</span>
+                <span>{{ lanMap["chaddr_check"] }}</span>
                 <select v-model.number="chaddr_check">
-                    <option :value="0">{{ lanMap['disable'] }}</option>
-                    <option :value="1">{{ lanMap['enable'] }}</option>
+                    <option :value="0">{{ lanMap["disable"] }}</option>
+                    <option :value="1">{{ lanMap["enable"] }}</option>
                 </select>
             </div>
             <div v-if="dialogType === 'time'" class="dhcp-dialog-content">
-                <span>{{ lanMap['response_time'] }}</span>
+                <span>{{ lanMap["response_time"] }}</span>
                 <input
                     type="text"
                     v-model="response_time"
-                    style="width: 100px;"
-                    :style="{ 'border-color': validTime || response_time === '' ? '' : 'red' }"
+                    style="width: 100px"
+                    :style="{
+                        'border-color':
+                            validTime || response_time === '' ? '' : 'red',
+                    }"
                 />
                 <span class="tips">Range: 3 - 3600 s</span>
             </div>
             <div v-if="dialogType === 'portlist'" class="dhcp-dialog-content">
-                <span style="width: 120px;" class="lf">{{ lanMap['trust_portlist'] }}:</span>
+                <span style="width: 120px" class="lf"
+                    >{{ lanMap["trust_portlist"] }}:</span
+                >
                 <div class="lf">
                     <template v-for="(item, index) in port_name.ge">
-                        <label :class="{ 'disabled': isDisable(item.id) }">
+                        <label :class="{ disabled: isDisable(item.id) }">
                             <input
                                 type="checkbox"
                                 v-model="trust_portlist"
@@ -87,7 +138,7 @@
                     </template>
                     <template v-if="port_name.xge">
                         <template v-for="item in port_name.xge">
-                            <label :class="{ 'disabled': isDisable(item.id) }">
+                            <label :class="{ disabled: isDisable(item.id) }">
                                 <input
                                     type="checkbox"
                                     v-model="trust_portlist"
@@ -101,8 +152,12 @@
                 </div>
             </div>
             <div slot="footer">
-                <a href="javascript: void(0);" @click="submitForm">{{ lanMap['apply'] }}</a>
-                <a href="javascript: void(0);" @click="closeDialog">{{ lanMap['cancel'] }}</a>
+                <a href="javascript: void(0);" @click="submitForm">{{
+                    lanMap["apply"]
+                }}</a>
+                <a href="javascript: void(0);" @click="closeDialog">{{
+                    lanMap["cancel"]
+                }}</a>
             </div>
         </nms-dialog>
     </div>
@@ -129,7 +184,7 @@ export default {
         },
         validTime() {
             return this.response_time >= 3 && this.response_time <= 3600;
-        }
+        },
     },
     data() {
         return {
@@ -141,14 +196,14 @@ export default {
             trust_portlist: [],
             //  dialog的 type:  admin / check / time / portlist
             dialogType: "",
-            dialogMethod: ""
+            dialogMethod: "",
         };
     },
     props: {
         globalData: {
             type: Object,
-            required: true
-        }
+            required: true,
+        },
     },
     created() {
         if (this.admin) {
@@ -172,14 +227,14 @@ export default {
             this.snoopingTable = [];
             this.$http
                 .get("/switch_dhcp?form=snooping_table")
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         if (res.data.data && res.data.data.length) {
                             this.snoopingTable = res.data.data;
                         }
                     }
                 })
-                .catch(err => {});
+                .catch((err) => {});
         },
         updateData() {
             this.$emit("update-data");
@@ -187,7 +242,7 @@ export default {
         postData(url, data) {
             this.$http
                 .post(url, data)
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         this.$message.success(
                             this.lanMap[data.method] + this.lanMap["st_success"]
@@ -199,7 +254,7 @@ export default {
                         );
                     }
                 })
-                .catch(err => {});
+                .catch((err) => {});
             this.closeDialog();
         },
         openDialog(flag, method = "add") {
@@ -220,14 +275,14 @@ export default {
         },
         clearSnoopingTable() {
             this.$confirm(this.lanMap["if_sure"])
-                .then(_ => {
+                .then((_) => {
                     const data = {
                         method: "set",
-                        param: {}
+                        param: {},
                     };
                     this.$http
                         .post("/switch_dhcp?form=snooping_flush", data)
-                        .then(res => {
+                        .then((res) => {
                             if (res.data.code === 1) {
                                 this.$message.success(
                                     this.lanMap["clear"] +
@@ -240,9 +295,9 @@ export default {
                                 );
                             }
                         })
-                        .catch(err => {});
+                        .catch((err) => {});
                 })
-                .catch(_ => {});
+                .catch((_) => {});
         },
         submitForm() {
             const actions = {
@@ -254,8 +309,8 @@ export default {
                     const data = {
                         method: "set",
                         param: {
-                            snooping_admin: this.snooping_admin
-                        }
+                            snooping_admin: this.snooping_admin,
+                        },
                     };
                     const url = "/switch_dhcp?form=snooping_admin";
                     this.postData(url, data);
@@ -268,8 +323,8 @@ export default {
                     const data = {
                         method: "set",
                         param: {
-                            chaddr_check: this.chaddr_check
-                        }
+                            chaddr_check: this.chaddr_check,
+                        },
                     };
                     const url = "/switch_dhcp?form=snooping_chaddr";
                     this.postData(url, data);
@@ -288,8 +343,8 @@ export default {
                     const data = {
                         method: "set",
                         param: {
-                            response_time: this.response_time
-                        }
+                            response_time: this.response_time,
+                        },
                     };
                     const url = "/switch_dhcp?form=snooping_response";
                     this.postData(url, data);
@@ -300,7 +355,7 @@ export default {
                         url = "/switch_dhcp?form=snooping_trust_add";
                         const portlist = parsePort(this.portlist);
                         list = this.trust_portlist.filter(
-                            item => !portlist.includes(item)
+                            (item) => !portlist.includes(item)
                         );
                     } else {
                         url = "/switch_dhcp?form=snooping_trust_del";
@@ -316,11 +371,11 @@ export default {
                     const data = {
                         method: "set",
                         param: {
-                            trust_portlist
-                        }
+                            trust_portlist,
+                        },
                     };
                     this.postData(url, data);
-                }
+                },
             };
             if (typeof actions[this.dialogType] === "function") {
                 actions[this.dialogType].call(this);
@@ -334,21 +389,21 @@ export default {
                 },
                 delete() {
                     return !list.includes(port_id);
-                }
+                },
             };
             return actions[this.dialogMethod].call(this);
         },
         refreshData() {
             debounce(this.getData, 1000, this, null);
-        }
+        },
     },
     watch: {
         admin() {
             if (this.admin) {
                 this.getData();
             }
-        }
-    }
+        },
+    },
 };
 </script>
 
